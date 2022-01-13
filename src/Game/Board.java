28fc557173c4e74ad46b79bc3fc5b92572e1e1d4
ngqhung0912 <67213674+ngqhung0912@users.noreply.github.com>
2022-01-13 16:16:30 +0100

    initial TUI

---
+++ src/Game/Board.java
@@ -0,0 +1,6 @@
+package Game;
+
+public class Board {
+    public final int SIZE = 15;
+
+}
---
+++ src/TUI/BoardConstructor.java
@@ -0,0 +1,51 @@
+package TUI;
+import Game.*;
+
+
+public class BoardConstructor  {
+    public static String generateBoard() {
+        Board board = new Board();
+        StringBuilder builder = new StringBuilder();
+        builder.append("    ");
+        for (int x = 0; x < board.SIZE; x++) {
+            builder.append("  " + ((char)65+x) + "  ");
+        }
+        builder.append("\n");
+
+        builder.append("┏");
+
+        for (int x = 0; x < board.SIZE-1; x++) {
+            builder.append("━━━━┳");
+        }
+        builder.append("━━━━┓");
+        builder.append("\n");
+        builder.append("┃");
+
+
+
+        for (int y = 0; y < board.SIZE; y++) {
+            builder.append("" + (y<9? "  " : " ") + (y+1) + " ");
+            builder.append("┃");
+
+            for (int x = 0; x < board.SIZE; x++) {
+                //implement switch case for color-coding special
+            }
+
+        }
+
+
+
+        return builder.toString();
+
+    }
+    public static void main(String[] args) {
+        System.out.println(generateBoard());
+    }
+
+
+
+
+
+
+
+}
