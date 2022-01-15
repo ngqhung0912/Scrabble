2a860f8ba8eda86f36be36df9f1a527c7cff3fdc
ngqhung0912 <67213674+ngqhung0912@users.noreply.github.com>
2022-01-15 10:32:33 +0100

    add visual paradigm gitignore

--- src/Game/Square.java
+++ src/Game/Square.java
@@ -4,10 +4,57 @@ public class Square {
     private TileType type;
     private Tile tile;
 
+    /**
+     *
+     * @param type
+     * @param tile
+     */
     public Square(TileType type, Tile tile) {
         this.tile = tile;
         this.type = type;
     }
-    
 
+    /**
+     *
+     * @return
+     */
+    public TileType getType() {
+        return type;
+    }
+
+    public void setTile(Tile tile) {
+        this.tile = tile;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Tile getTile() {
+        return tile;
+    }
+
+    /**
+     *
+     * @param type
+     */
+    public void setType(TileType type) {
+        this.type = type;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean hasTile() {
+        return false;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        return false;
+    }
 }
