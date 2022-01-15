0c1dec6245e52bcb267dc06a80b31bbf0fd2f658
ngqhung0912 <67213674+ngqhung0912@users.noreply.github.com>
2022-01-15 10:02:29 +0100

    add gitignore

---
+++ src/Game/Square.java
@@ -0,0 +1,13 @@
+package Game;
+
+public class Square {
+    private TileType type;
+    private Tile tile;
+
+    public Square(TileType type, Tile tile) {
+        this.tile = tile;
+        this.type = type;
+    }
+    
+
+}
