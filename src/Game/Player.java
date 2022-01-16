a00246055b9886126f2ecadeabd728289d1a0253
nhattran146 <nhattran14602@gmail.com>
2022-01-16 16:31:47 +0100

    implement Board, Game, Player, Square

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,31 +1,61 @@
 package Game;
 
-public abstract class Player {
-     private String name;
+/**
+ * author Nhat Tran
+ */
+public class Player {
 
+     // -- Instance variables -----------------------------------------
+
+     private String name;
      private int point;
      private Tile[] tray;
 
+     // -- Constructors -----------------------------------------------
+
+     public Player(String name, Tile[] tray){
+          this.name = name;
+          point = 0;
+          this.tray = tray;
+     }
+
+     /**
+      * Get the name of the player
+      * @return player's name
+      */
      public String getName() {
           return name;
      }
 
-     public void setName(String name) {
-          this.name = name;
-     }
-
-
+     /**
+      * Return the tray with the current tiles belongs to the player
+      * @return tray - The tray with the current tiles belongs to the player
+      */
      public Tile[] getTray() {
           return tray;
      }
 
+     /**
+      * Set the tray of the player with new tile(s)
+      * @param tray
+      */
      public void setTray(Tile[] tray) {
           this.tray = tray;
      }
 
+     /**
+      * Get the current point of the player
+      * @return
+      */
      public int getPoint() {
           return point;
      }
 
-     public abstract void setPoint(int point);
+     /**
+      *
+      * @param point
+      */
+     public void setPoint(int point){
+          this.point = point;
+     }
 }
