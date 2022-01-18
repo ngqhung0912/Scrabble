fec5929cdf9805db2e1a8a9151e0f5c73fb4c824
ngqhung0912 <67213674+ngqhung0912@users.noreply.github.com>
2022-01-18 14:29:27 +0100

    fix small bugs

--- src/Game/Controller.java
+++ src/Game/Controller.java
@@ -1,6 +1,30 @@
 package Game;
+import TUI.BoardConstructor;
+import TUI.BoardConstructor.*;
 
 public class Controller {
+
+    private Game game;
+
+
+    public Controller() {
+        Game game = new Game;
+        BoardConstructor.generateBoard()
+
+    }
+
+
+    public void start(){
+    }
+
+    public void update(){
+
+    }
+
+    public void printResult(){
+
+    }
+
 
 
 
