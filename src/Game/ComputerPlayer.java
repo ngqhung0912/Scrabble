9af6459ddf90f6f7cde29bb2cc603fd9f2328d3d
ngqhung0912 <67213674+ngqhung0912@users.noreply.github.com>
2022-01-19 10:03:03 +0100

    add javadoc for game.java

--- src/Game/ComputerPlayer.java
+++ src/Game/ComputerPlayer.java
@@ -8,6 +8,17 @@ public class ComputerPlayer extends Player{
         super(name, tray);
     }
 
+    public void getPotentialMoves() {
+
+    }
+
+    public String determineMove() {
+
+        return null;
+    }
+
+
+
 
 
 
