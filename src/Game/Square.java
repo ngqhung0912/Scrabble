f7c78212f3c1fae1fb5a8aca52e9407e36655cca
nhattran146 <nhattran14602@gmail.com>
2022-01-19 14:06:11 +0100

    add coordinate to Square

--- src/Game/Square.java
+++ src/Game/Square.java
@@ -7,14 +7,18 @@ package Game;
 public class Square {
     private SquareType type;
     private Tile tile;
+    private int xPosition;
+    private int yPosition;
 
     /**
      * Square Constructor
      * @param type of the square, indicated by SquareType enum
      *
      */
-    public Square(SquareType type) {
+    public Square(SquareType type, int xPosition, int yPosition) {
         this.type = type;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
     }
 
     /**
